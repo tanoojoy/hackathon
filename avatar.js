5bad027dabe967a9c949150ccc4f97362bccf6f5
Tanoo <tanoo@arcadier.com>
2024-06-19 21:50:54 +0400

    harshini files

---
+++ avatar.js
@@ -0,0 +1,18 @@
+let selectedImage = null;
+
+function selectImage(imgElement) {
+    if (selectedImage) {
+        selectedImage.classList.remove('selected');
+    }
+    selectedImage = imgElement;
+    selectedImage.classList.add('selected');
+}
+
+function confirmSelection() {
+    if (selectedImage) {
+        window.location.href = 'rules.html'; 
+    } else { 
+        alert('No image selected.');
+    }
+}
+
